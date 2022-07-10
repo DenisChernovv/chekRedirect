9329ef28b350dd78cbab21aad4ff6d0d166cc690
Denis Chernov <naantarktide@gmail.com>
2022-07-10 21:25:42 +0700

    1

--- index.js
+++ index.js
@@ -1,18 +1,82 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
+const request = require("request");
+const https = require('https');
 // const {performance} = require('node:perf_hooks');
+
+let listUrls = process.argv;
+
+
+
+
+
+async function main(){
+    let mass = [];
+    let dataFile = getDataFromFile();
+    console.log(dataFile);
+
+    let startUrl, endUrl, getUrl = '';
+    for (let i = 0; i < dataFile.length; i++){
+        [startUrl, endUrl] = dataFile[i].split('\t');
+        getUrl = await callCurl(startUrl);
+        // if (getUrl !== endUrl) {
+        //     mass.push([getUrl, endUrl]);
+            // console.log('%cNOT_MATCHING', 'color:red;');
+            // console.log("get = " + getUrl);
+            // console.log("expect = " + endUrl);
+        // }       
+    }
+
+    console.log(mass.map( (element) => {
+        return ['get + ' + element[0], 'expect + ' + element[1]];
+    }));
+    // time = performance.now() - time;
+    // console.log('Время выполнения = ', time);
+    // getUrl = await callCurl(startUrl);
+    // console.log(endUrl === getUrl);
+    // console.log("get = " + getUrl);
+    // console.log("end = " + endUrl);
+    // console.log("start = " + startUrl);
+};
+
+function getDataFromFile() {
+    let data = fs.readFileSync('C:\\Users\\admin\\Desktop\\checkRedirect\\urlForTest.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    return data.split('\n');
+}
+main();
 
 
 
 async function callCurl (startUrl){
     try {
         let we = execSync(`curl -Ls -o /dev/null -w %{url_effective} ${startUrl}`,
+
         function (error, stdout, stderr) {
             console.log('stderr: ' + stderr);
             if (error !== null) {
-                 console.log('exec error: ' + error);
+                console.log('exec error: ' + error);
             }
         });
+        // let we = execSync('echo $SHELL',
+        // function (error, stdout, stderr) {
+        //     console.log('stderr: ' + stderr);
+        //     if (error !== null) {
+        //         console.log('exec error: ' + error);
+        //     }
+        // });
+        // var r = await request(startUrl, function (e, res) {
+        //             console.log(res);
+        //   });
+        // let response = await fetch(startUrl, {
+        //     method: 'GET',
+        //  });
+        console.log()
+
         return we.toString();
     } catch (error) {
         console.log(error.message);
@@ -21,48 +85,6 @@ async function callCurl (startUrl){
     }
 
 }
-
-
-async function main(){
-    let mass = [];
-    console.time('script')
-    let data = fs.readFileSync('/Users/sibirixdeveloper/Desktop/S3.txt', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      });
-      
-    let arr = data.split('\n');
-    
-    let startUrl, endUrl, getUrl = '';
-    for (let i = 0; i < 5; i++){
-        [startUrl, endUrl] = arr[i].split('\t');
-        getUrl = await callCurl(startUrl);
-
-        if (getUrl !== endUrl) {
-            mass.push([getUrl, endUrl]);
-            // console.log('%cNOT_MATCHING', 'color:red;');
-            // console.log("get = " + getUrl);
-            // console.log("expect = " + endUrl);
-        }       
-    }
-
-    console.log(mass.map( (element) => {
-        return ['get + ' + element[0], 'expect + ' + element[1]];
-    }));
-    // time = performance.now() - time;
-    // console.log('Время выполнения = ', time);
-    console.timeEnd('script');
-    // getUrl = await callCurl(startUrl);
-    // console.log(endUrl === getUrl);
-    // console.log("get = " + getUrl);
-    // console.log("end = " + endUrl);
-    // console.log("start = " + startUrl);
-};
-
-main();
-
 
 
 
